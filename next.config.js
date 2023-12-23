73b3c2a5cf498a580f4e9c18cdf5be83e2d7dd82
Harry Tasker <harry@taskersolutions.co.nz>
2023-12-24 07:25:08 +1300

    deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
 /** @type {import('next').NextConfig} */
 
-module.exports = {
-
-    images: {
-      // Add this configuration to disable Image Optimization API
-      unoptimized: true,
-    },
-  };
+module.exports = {};
