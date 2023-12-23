5b078d66fefff691effa789a2d790ab12a2f4acf
Harry Tasker <harry@taskersolutions.co.nz>
2023-12-24 07:10:17 +1300

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  basePath: "/ts-portfolio",
     images: {
       // Add this configuration to disable Image Optimization API
       unoptimized: true,
