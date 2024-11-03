3ae7038355b3959a588c38c620b7575635899dae
Harry <harry@bocapa.com>
2024-11-04 08:31:32 +1300

    update node

--- components/Aboutme.tsx
+++ components/Aboutme.tsx
@@ -8,7 +8,7 @@ const Aboutme = () => {
       <div className="text-lg tracking-wide flex flex-col gap-6">
         <p>
           I'm a Kiwi, who's passionate about learning, happiness and health.<br></br>
-          Obsessed with AI and optimistic about the future!<br></br>
+          Obsessed with AI and optimistic about the future!
         </p>
         <a href="mailto:harry@taskersolutions.co.nz" target="_blank">
           <button className="w-52 h-14 bg-gradient-to-r from-blue-600 via-blue-700 to-indigo-950 text-xl font-bold text-white rounded-lg hover:from-indigo-950 hover:to-blue-600 duration-300">
